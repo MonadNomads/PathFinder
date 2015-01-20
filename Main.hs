b870ef7d3f9a3196c318fb9b44fd003b2123ea6e
Nicuvëo <crucuny@gmail.com>
2015-01-20 13:42:48 +0100

    Coded path! Yay!

--- Main.hs
+++ Main.hs
@@ -2,13 +2,14 @@
 
 
 
-import Data.List
+import Data.List as L hiding (insert)
+import Data.Map  as M hiding ((\\))
 
 
 
-data Node  a = Eq a => Node { label :: a }
-data Edge  a = Eq a => Edge a a
-data Graph a =         Graph { nodes :: [Node a], edges :: [Edge a] } deriving Show
+data Node  a = Ord a => Node { label :: a }
+data Edge  a = Ord a => Edge a a
+data Graph a =          Graph { nodes :: [Node a], edges :: [Edge a] } deriving Show
 
 
 
@@ -23,8 +24,29 @@ instance Show a => Show (Edge a) where
 node :: Graph a -> a -> Maybe (Node a)
 node g x = find (\(Node y) -> y == x) (nodes g)
 
-path :: Graph a -> a -> a -> Maybe [a]
-path g from to = undefined
+path :: Ord a => Graph a -> a -> a -> Maybe [a]
+path g from to = path' g from to (singleton to (0, Nothing))
+
+path' g f t m
+  | f `member` m = Just $ build_path m f
+  | otherwise    = path' g f t (L.foldr (\n -> extend g n) m (keys m))
+                   -- will recurse indefinitely if there is no path
+
+build_path :: Ord a => Map a (Int, Maybe a) -> a -> [a]
+build_path m n = let (_, mp) = m ! n in
+                  case mp of
+                   Nothing -> [n]
+                   Just p  -> n : build_path m p
+
+extend :: Ord a => Graph a -> a -> Map a (Int, Maybe a) -> Map a (Int, Maybe a)
+extend g n m =
+  let (l, _) = m ! n
+      ps = precs g n
+      ns = ps \\ (keys m) in
+  L.foldr (\p -> insert p (l + 1, Just n)) m ns
+
+precs :: Graph a -> a -> [a]
+precs g n = [f | Edge f t <- (edges g), t == n]
 
 
 
@@ -33,4 +55,4 @@ example = Graph [Node "Vendome", Node "Opera", Node "Couloir"]
                 [Edge "Vendome" "Couloir",
                  Edge "Couloir" "Opera"]
 
-main = print example
+main = print $ path example "Vendome" "Opera"
