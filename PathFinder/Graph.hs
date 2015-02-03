c2b8681c1ec21055585697b65746b93816e80b07
Nicuvëo <crucuny@gmail.com>
2015-02-03 14:06:46 +0100

    Removed ExistentialQuantification.

--- PathFinder/Graph.hs
+++ PathFinder/Graph.hs
@@ -1,12 +1,10 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module PathFinder.Graph where
 
 import Data.List
 
-data Node  a = Ord a => Node { label :: a }
-data Edge  a = Ord a => Edge a a
-data Graph a =          Graph { nodes :: [Node a], edges :: [Edge a] } deriving Show
+data Node  a = Node { label :: a }
+data Edge  a = Edge a a
+data Graph a = Graph { nodes :: [Node a], edges :: [Edge a] } deriving Show
 
 instance Show a => Show (Node a) where
   show (Node x) = show x
@@ -14,8 +12,8 @@ instance Show a => Show (Node a) where
 instance Show a => Show (Edge a) where
   show (Edge x y) = show x ++ " -> " ++ show y
 
-node :: Graph a -> a -> Maybe (Node a)
+node :: Eq a => Graph a -> a -> Maybe (Node a)
 node g x = find (\(Node y) -> y == x) (nodes g)
 
-parents :: Graph a -> a -> [a]
+parents :: Eq a => Graph a -> a -> [a]
 parents g n = [f | Edge f t <- edges g, t == n]
