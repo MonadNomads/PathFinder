d4dc181addc85805898c2586b67c65f98cc55cd7
Nicuvëo <crucuny@gmail.com>
2015-02-03 23:43:37 +0100

    Added brand new test.

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,35 +1,41 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+
+import System.Exit
+import Data.List
 import Data.Maybe
 import Control.Monad
 import Control.Applicative
 import Test.QuickCheck
+import Test.QuickCheck.All
+import Test.QuickCheck.Test
 import PathFinder.Graph as G
 import PathFinder.PathFinder
 
-newtype ConnectedGraph    a = CG { cg :: Graph a }
-newtype DisconnectedGraph a = DG { dg :: Graph a }
 
-instance Arbitrary a => Arbitrary (ConnectedGraph a) where
-  arbitrary = join (foldr build start <$> labels)
-    where labels        = arbitrary `suchThat` isBigEnough -- Gen [a]
-          build l       = (>>= addToCG l)                  -- a -> Gen (ConnectedGraph a) -> Gen (ConnectedGraph a)
-          start         = return (CG G.empty)              -- Gen (ConnectedGraph a)
-          isBigEnough x = length x > 2                     -- [a] -> Bool
 
-addToCG :: a -> ConnectedGraph a -> Gen (ConnectedGraph a)
-addToCG newLabel connectedGraph
-  | null (nodes graph) = return singleton
-  | otherwise          = fmap buildGraph oldLabelGen
-  where graph        = cg connectedGraph                               -- Graph a
-        singleton    = CG $ Graph [Node newLabel] []                   -- ConnectedGraph a
-        oldLabelGen  = G.label <$> elements (nodes graph)              -- Gen a
-        newNodes     = Node newLabel : nodes graph                     -- [Node a]
-        newEdges l   = Edge l newLabel : Edge newLabel l : edges graph -- a -> [Edge a]
-        buildGraph l = CG $ Graph newNodes (newEdges l)                -- a -> ConnectedGraph a
+instance (Arbitrary a, Eq a) => Arbitrary (Graph a) where
+  arbitrary = do
+    nodes <- nub <$> listOf1 arbitrary
+    esize <- arbitrary `suchThat` (< 2 * length nodes)
+    edges <- (nub . concat) <$> (vectorOf esize
+                                 (do f <- elements nodes
+                                     t <- elements nodes `suchThat` (/= f)
+                                     return [(f, t), (t, f)]))
+    return $ Graph (map Node nodes) (map (uncurry Edge) edges)
 
-nodesWorks :: [String] -> String -> Bool
-nodesWorks ls l = (l `elem` ls) == isJust (node g l)
+
+
+prop_nodesWorks :: Eq a => [a] -> a -> Bool
+prop_nodesWorks ls l = (l `elem` ls) == isJust (node g l)
   where ns = fmap Node ls
         g = Graph ns []
 
-test = quickCheck . verbose
-main = test nodesWorks
+prop_connectedPaths :: Ord a => Graph a -> Bool
+prop_connectedPaths g = isConnected g == all isJust [path g x y | (Node x) <- nodes g, (Node y) <- nodes g]
+
+
+
+main = do
+  result <- $quickCheckAll
+  unless result exitFailure
