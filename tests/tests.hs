f4da934d23a2abd8c3978fa52ccdd923d576c77c
Nicuvëo <crucuny@gmail.com>
2015-02-03 15:19:01 +0100

    Hlint cleaning.

--- tests/tests.hs
+++ tests/tests.hs
@@ -5,8 +5,8 @@ import Test.QuickCheck
 import PathFinder.Graph as G
 import PathFinder.PathFinder
 
-newtype ConnectedGraph    a = CG { cg :: (Graph a) }
-newtype DisconnectedGraph a = DG { dg :: (Graph a) }
+newtype ConnectedGraph    a = CG { cg :: Graph a }
+newtype DisconnectedGraph a = DG { dg :: Graph a }
 
 instance Arbitrary a => Arbitrary (ConnectedGraph a) where
   arbitrary = join (foldr build start <$> labels)
@@ -19,12 +19,12 @@ addToCG :: a -> ConnectedGraph a -> Gen (ConnectedGraph a)
 addToCG newLabel connectedGraph
   | null (nodes $ cg connectedGraph) = return singleton
   | otherwise                        = fmap buildGraph oldLabelGen
-  where graph        = cg connectedGraph                                   -- Graph a
-        singleton    = CG $ Graph [Node newLabel] []                       -- ConnectedGraph a
-        oldLabelGen  = fmap G.label $ elements (nodes graph)               -- Gen a
-        newNodes     = Node newLabel : nodes graph                         -- [Node a]
-        newEdges l   = (Edge l newLabel) : (Edge newLabel l) : edges graph -- a -> [Edge a]
-        buildGraph l = CG $ Graph newNodes (newEdges l)                    -- a -> ConnectedGraph a
+  where graph        = cg connectedGraph                               -- Graph a
+        singleton    = CG $ Graph [Node newLabel] []                   -- ConnectedGraph a
+        oldLabelGen  = G.label <$> elements (nodes graph)              -- Gen a
+        newNodes     = Node newLabel : nodes graph                     -- [Node a]
+        newEdges l   = Edge l newLabel : Edge newLabel l : edges graph -- a -> [Edge a]
+        buildGraph l = CG $ Graph newNodes (newEdges l)                -- a -> ConnectedGraph a
 
 nodesWorks :: [String] -> String -> Bool
 nodesWorks ls l = (l `elem` ls) == isJust (node g l)
